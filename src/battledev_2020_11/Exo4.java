ebc7d10368c2fa01792bbf6bd6bc11b7f0dfcd63
Kyung Mo Yang <kyungmoyang9091.92@gmail.com>
2020-11-28 17:43:39 +0100

    exo 4 is still unsatisfying

--- src/battledev_2020_11/Exo4.java
+++ src/battledev_2020_11/Exo4.java
@@ -1,5 +1,11 @@
 package battledev_2020_11;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+
 /*
  * Après avoir correctement repéré la structure du réseau, vous accédez à un salon privé de discussion dont le contenu est chiffré de manière très étrange : chaque message peut être déchiffré en effectuant plusieurs opérations sur une clé initiale de N octets partagé entre tous les utilisateurs.
 
@@ -51,9 +57,92 @@ Le message déchiffré est donc : 27 29 33 58, d'où la sortie attendue
 
 public class Exo4 {
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String  line;
+		Scanner sc = new Scanner(System.in);
+		int index = 0, n = 0;
+		List<Integer> keys = new ArrayList<>();
+		Map<Integer, Integer>result = new HashMap<>();
+		int maxXor = 0;
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+			/* si index = 0 lire m et n
+			 * si index = 1 stocker les cles
+			 * sinon
+			 * 		pour chaque indice
+			 * 			calcul et stockage de xor  */
+			if (index == 0) {
+			} else if (index == 1) {
+				for (String string : line.split(" ")) {
+					keys.add(Integer.parseInt(string));
+				}
+				maxXor = xor(keys);
+				n = keys.size();
+			} else {
+				String[] split = line.split(" ");
+				int min = Integer.parseInt(split[0]);
+				int max = Integer.parseInt(split[1]);
+				int res = 0;
+				if (max - min < n / 2) {
+					res = xor(keys.subList(min, max + 1));
+				} else {
+					res = maxXor
+							^ xor(keys.subList(0, min))
+							^ xor(keys.subList(max + 1, keys.size()));
+				}
+				if (!result.containsKey(res)) {
+					result.put(res,  0);
+				}
+				result.put(res, result.get(res) + 1);
+			}
+			index ++;
+		}
+	/* initialiser sb
+	 * pour i de 0 à 255
+	 * 		si map contient i, stocker sa valeur
+	 * 		sinon stocker 0 */
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 256; i++) {
+			if (result.containsKey(i)) {
+				sb.append(result.get(i) + " ");
+			} else {
+				sb.append("0 ");
+			}
+		}
+		System.out.println(sb.toString().trim());
 	}
 
+	private static int xor(List<Integer> keys) {
+		if (keys.isEmpty()) return 0;
+		if (keys.size() == 1) return keys.get(0);
+		if (keys.size() == 2) return keys.get(0) ^ keys.get(1);
+		Map<Integer, Integer> temp = new HashMap<>();
+		for (Integer integer : keys) {
+			if (integer != 0) {
+				if (!temp.containsKey(integer)) {
+					temp.put(integer, 0);
+				}
+				temp.put(integer, temp.get(integer) + 1);
+			}
+		}
+		boolean paired = true;
+		int toPair = 0;
+		List<Integer> list = new ArrayList<>();
+		for (Integer key : temp.keySet()) {
+			if (temp.get(key) % 2 == 1) {
+				if (paired) {
+					toPair = key;
+					paired = false;
+				} else {
+					list.add(toPair ^ key);
+					paired = true;
+				}
+			}
+		}
+		if (!paired) {
+			list.add(toPair);
+		}
+		return xor(list);
+	}
 }
