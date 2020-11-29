a4952bfb65a20d88086e7010333ba1b84510b41f
Kyung Mo Yang <kyungmoyang9091.92@gmail.com>
2020-11-29 02:11:39 +0100

    exo 5

--- src/battledev_2020_11/Exo5.java
+++ src/battledev_2020_11/Exo5.java
@@ -1,5 +1,10 @@
 package battledev_2020_11;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
 /*
  * En interceptant les messages du réseau de Dolan Grump, vous êtes parvenu à obtenir le lien vers une plateforme qui semble coordonner les actions de fake news. Malheureusement, la création de compte invité ne vous permet pas d'accéder aux contenus les plus intéressants du site, il va falloir ruser pour obtenir les bonnes autorisations...
 
@@ -37,10 +42,76 @@ En choisissant le pseudonyme Coolh4cker0780578, votre hash sera également 154896
  * */
 
 public class Exo5 {
+	
+	static final long LAMBDA = 4294967296l, MIN = 33, MAX = 126, DIV = 31;
 
+	@SuppressWarnings("resource")
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		String  line;
+		Scanner sc = new Scanner(System.in);
+		long sigma = 0;
+		while(sc.hasNextLine()) {
+			line = sc.nextLine();
+			System.err.println("line = " + line);
+			sigma = modulo(line) + LAMBDA;
+			/* changer le line en chaine d'entier ascii
+			 * calculer la somme totale et la valeur cible
+			 * 		-> remplacé par son modulo pour l'espace
+			 * calculer sigma la nouvelle somme avec lambda */
+		}
+		/* tant que sigma n'est pas comprise entre 33 et 126,
+		 * 		calculer le reste de sigma par le diviseur
+		 * 		tant que u n'est pas compris entre 33 et 126
+		 * 			u = u + diviseur
+		 * 		ajouter u à la liste
+		 * 		sigma = (sigma - u) / diviseur
+		 * retourner la liste
+		 * convertir en ascii
+		 * sb */
+		List<Long> inverseList = new ArrayList<>();
+		System.err.println("sigma = " + sigma);
+		while (sigma > MAX) {
+			long u = sigma % DIV;
+			while (u < MIN) {
+				u += DIV;
+			}
+			inverseList.add(u);
+			System.err.println("u = " + u);
+			sigma = (sigma - u) / DIV;
+			System.err.println("sigma = " + sigma);
+		}
+		inverseList.add(sigma);
+		System.out.println(inverseToASCII(inverseList));
 	}
 
+	private static long modulo(String line) {
+		char[] charArray = line.toCharArray();
+		int n = charArray.length;
+		long result = 0;
+		for (int i = 0; i < n; i++) {
+			result += ((long) charArray[n - i - 1] * powerFacto(i) % LAMBDA) % LAMBDA;
+		}
+		return result % LAMBDA;
+	}
+
+	private static long powerFacto(int i) {
+		if (i == 0) {
+			return 1;
+		}
+		if (i == 1) {
+			return DIV;
+		}
+		if (i % 2 == 0) {
+			return ((powerFacto(i / 2) % LAMBDA) * (powerFacto(i / 2) % LAMBDA) % LAMBDA);
+		}
+		return DIV * ((powerFacto(i / 2) % LAMBDA) * (powerFacto(i / 2) % LAMBDA) % LAMBDA);
+	}
+
+	private static String inverseToASCII(List<Long> inverseList) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = inverseList.size() - 1; i >= 0; i--) {
+			sb.append((char) (long) inverseList.get(i));
+		}
+		return sb.toString();
+	}
 }
